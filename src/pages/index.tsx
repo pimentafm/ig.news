775f54811d4d0ea8d1d18d3a25d82b1a06079307
Fernando Pimenta <fernando.m.pimenta@gmail.com>
2021-04-25 15:55:00 -0300

    Styling homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,27 @@
 import Head from "next/head";
 
+import styles from "./home.module.scss";
+
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Inicio | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
-      <h1>
-        Hello <span>World</span>
-      </h1>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            News about the <span>React</span>
+          </h1>
+          <p>
+            Get access to all the puvlications <br />
+            <span>for $9.90</span>
+          </p>
+        </section>
+        <img src="/images/avatar.svg"></img>
+      </main>
     </>
   );
 }
