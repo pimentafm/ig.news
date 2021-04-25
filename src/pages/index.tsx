68bc57b8072f6cc48e38827271f61f1f23681a56
Fernando Pimenta <fernando.m.pimenta@gmail.com>
2021-04-25 16:03:19 -0300

    Adding subscribe button component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import { SubscribeButton } from "../components/SubscribeButton";
 
 import styles from "./home.module.scss";
 
@@ -19,6 +20,7 @@ export default function Home() {
             Get access to all the puvlications <br />
             <span>for $9.90</span>
           </p>
+          <SubscribeButton />
         </section>
         <img src="/images/avatar.svg"></img>
       </main>
