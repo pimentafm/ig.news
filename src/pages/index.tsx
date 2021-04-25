2e602ab4c156f0a4d25c4ddc97c6517162c2e727
Fernando Pimenta <fernando.m.pimenta@gmail.com>
2021-04-25 18:09:24 -0300

    Adding nextauth

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 import Head from "next/head";
 import { SubscribeButton } from "../components/SubscribeButton";
 import { stripe } from "../services/stripe";
@@ -37,7 +37,7 @@ export default function Home({ product }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve("price_1IkDKeGt4neGrnYJwGNSRAS6", {
     expand: ["product"],
   });
@@ -52,5 +52,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
 
   return {
     props: { product },
+    revalidate: 60 * 60 * 24,
   };
 };
